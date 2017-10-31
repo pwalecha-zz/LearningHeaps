58fa29fc35d33bab3811cf065c3d17bad11d6c9f
pwalecha <pwalecha@gmail.com>
2017-10-31 08:44:10 +0530

    Min Heap Implementation

---
+++ LearningHeap/LearningHeap/HeapByArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningHeap
+{
+    public class HeapByArray
+    {
+        private int[] array;
+        private int currentPosition = 0;
+        public HeapByArray()
+        {
+            array = new int[20];
+        }
+
+        private int GetLeftChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 1;
+        }
+
+        private int GetRightChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 2;
+        }
+
+        private int GetParentIndexFromChild(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+
+        public void Insert (int value)
+        {
+            if(array[0] == 0)
+            {
+                array[currentPosition] = value;
+            }
+            else
+            {
+                array[currentPosition] = value;
+
+                for(int i = currentPosition + 1; i > 0; i = i/ 2)
+                {
+                    int pIndex = GetParentIndexFromChild(currentPosition);
+                    if(array[pIndex] > array[currentPosition]){
+                        int temp = array[pIndex];
+                        array[pIndex] = value;
+                        array[currentPosition] = temp;
+                    }
+                }
+            }
+
+            currentPosition++;
+        }
+
+        public void Delete(int value)
+        {
+
+        }
+    }
+}
---
+++ LearningHeap/LearningHeap/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HeapByArray heap = new HeapByArray();
+            heap.Insert(1);
+            heap.Insert(3);
+            heap.Insert(4);
+            heap.Insert(2);
+            heap.Insert(5);
+            heap.Insert(6);
+            heap.Insert(7);
+
+            Console.Read();
+        }
+    }
+}
